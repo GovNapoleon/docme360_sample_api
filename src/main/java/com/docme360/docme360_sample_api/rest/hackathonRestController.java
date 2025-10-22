caea0cf23833d2cf64174451190d3c87a23a5d99
Napoleon K. <95312667+GcioNapoleon@users.noreply.github.com>
2025-10-22 08:20:17 -0500

    add swagger and json-simple to pom.xml. Create a Hello endpoint.

--- src/main/java/com/docme360/docme360_sample_api/rest/hackathonRestController.java
+++ src/main/java/com/docme360/docme360_sample_api/rest/hackathonRestController.java
@@ -1,10 +1,18 @@
 package com.docme360.docme360_sample_api.rest;
 
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v0")
 public class hackathonRestController {
+    @GetMapping("/hello")
+    public JSONObject hello(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("message", "hello DOCME360 hackathon");
+        return jsonObject;
+    }
 
 }
