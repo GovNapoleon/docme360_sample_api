a3c5bdf83a18779c5e29078abc7654a0c09b2c98
Napoleon K. <95312667+GcioNapoleon@users.noreply.github.com>
2025-10-22 08:48:13 -0500

    Create a calculator restor controller. Create a sample Addition endpoint. Leave room for subtract, multiply, and division.

---
+++ src/main/java/com/docme360/docme360_sample_api/rest/calculatorRestController.java
@@ -0,0 +1,20 @@
+package com.docme360.docme360_sample_api.rest;
+
+
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class calculatorRestController {
+    @GetMapping("/add")
+    public JSONObject add(@RequestParam Integer a, @RequestParam Integer b) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("input", String.format("%d + %d",a,b));
+        jsonObject.put("output", a + b);
+        return jsonObject;
+    }
+}
--- src/main/java/com/docme360/docme360_sample_api/rest/hackathonRestController.java
+++ src/main/java/com/docme360/docme360_sample_api/rest/hackathonRestController.java
@@ -2,7 +2,6 @@ package com.docme360.docme360_sample_api.rest;
 
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
