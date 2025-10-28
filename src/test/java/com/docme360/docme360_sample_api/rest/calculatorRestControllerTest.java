d789b59c8c665ffde689bd861ff310889099e2f1
Napoleon K. <95312667+GcioNapoleon@users.noreply.github.com>
2025-10-28 13:53:38 -0500

    Create class for testing the calculator api

---
+++ src/test/java/com/docme360/docme360_sample_api/rest/calculatorRestControllerTest.java
@@ -0,0 +1,24 @@
+package com.docme360.docme360_sample_api.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculatorRestControllerTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
